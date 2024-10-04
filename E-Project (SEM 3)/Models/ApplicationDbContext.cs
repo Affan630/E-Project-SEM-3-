369f06855ba342266e812edfa4fe54792ca15abf
affan630 <affanmansoor630@gmail.com>
2024-10-04 23:02:37 +0500

    dbcontext set

---
+++ E-Project (SEM 3)/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace E_Project__SEM_3_.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<StationeryItem> StationeryItems { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Hierarchy> Hierarchies { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<AuditLog> AuditLogs { get; set; }
+    }
+}
