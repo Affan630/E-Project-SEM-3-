d83c58447bbf5929c2a9554cd1277744fb4dd649
affan630 <affanmansoor630@gmail.com>
2024-10-04 18:08:25 +0500

    Add project files.

---
+++ E-Project (SEM 3)/Models/audit log.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class AuditLog
+{
+    [Key]
+    public int LogId { get; set; }
+
+    public int UserId { get; set; }
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Action { get; set; }
+
+    public string ActionDetails { get; set; }
+
+    public DateTime ActionTime { get; set; } = DateTime.Now;
+}
---
+++ E-Project (SEM 3)/Models/role.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Role
+{
+    [Key]
+    public int RoleId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string RoleName { get; set; }
+
+    [Required]
+    public decimal MaxBudget { get; set; }
+
+    public ICollection<User> Users { get; set; }
+
+    public ICollection<Hierarchy> Hierarchies { get; set; }
+    public ICollection<Hierarchy> SuperiorRoles { get; set; }
+}
